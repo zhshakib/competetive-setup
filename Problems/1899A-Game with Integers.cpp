56bd0aea5a457a86e88418db3cf7b6b14860caa0
zhshakib <zhshakib@gmail.com>
2023-11-19 00:23:25 +0600

    Problem Added!

---
+++ Problems/1899A-Game with Integers.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO()                      \
+  {                                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);                      \
+  }
+
+void solve();
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("io/input.txt", "r", stdin);
+  freopen("io/output.txt", "w", stdout);
+#endif
+  fastIO();
+
+  int T = 1;
+  cin >> T;  // Toggle for testCases!
+  while (T--) {
+    solve();
+  }
+  return 0;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << (n % 3 != 0 ? "First" : "Second") << '\n';
+}
