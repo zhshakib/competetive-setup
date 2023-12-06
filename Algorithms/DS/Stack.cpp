f20655e03a1539774f943c278f8161915ad8b1cf
zhshakib <zhshakib@gmail.com>
2023-12-07 01:41:39 +0600

    DS: Stack

---
+++ Algorithms/DS/Stack.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define MAX_LEN 100
+
+struct Stack {
+  int top;
+  int data[MAX_LEN];
+};
+
+void push(Stack *s, int item) {
+  if (s->top < MAX_LEN) {
+    s->data[s->top] = item;
+    s->top++;
+  } else {
+    printf("Stack overflow\n");
+  }
+}
+
+void pop(Stack *s) {
+  if (s->top > 0) {
+    s->top--;
+  } else {
+    printf("Stack underflow\n");
+  }
+}
+
+int length(Stack s) { return s.top; }
+
+int main() {
+  Stack height;
+  height.top = 0;
+  push(&height, 1);
+  push(&height, 2);
+  push(&height, 3);
+
+  for (int i = 0; i < 3; i++) {
+    printf("%d\n", height.data[i]);
+  }
+
+  printf("%d \n", length(height));
+  return 0;
+}
