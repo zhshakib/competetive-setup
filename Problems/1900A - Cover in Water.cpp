707c0757e467641beca94e68772f0f6bb9f7f836
zhshakib <zhshakib@gmail.com>
2023-12-07 01:53:51 +0600

    1900A - Cover in Water

---
+++ Problems/1900A - Cover in Water.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastIO()                      \
+  {                                   \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);                      \
+  }
+
+void solve();
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("io/input.txt", "r", stdin);
+  freopen("io/output.txt", "w", stdout);
+#endif
+  fastIO();
+
+  int T = 1;
+  // cin >> T;  // Toggle for testCases!
+  while (T--) {
+    solve();
+  }
+  return 0;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  int cubes[n];
+  for (int i = 0; i < n; ++i) {
+    cin >> cubes[i];
+  }
+
+  sort(cubes, cubes + n);
+
+  for (int i = 0; i < n; ++i) {
+    cout << cubes[i] << " ";
+  }
+}
