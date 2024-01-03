9117e13e264d10a288b3472371782ad322f1ab58
zhshakib <zhshakib@gmail.com>
2024-01-04 01:59:46 +0600

    Chore: Linked List, InsertLast added

--- Algorithms/DS/Linked_List.cpp
+++ Algorithms/DS/Linked_List.cpp
@@ -13,6 +13,7 @@ struct node {
 struct node *head = NULL;
 struct node *current = NULL;
 
+// Inserts a element first and make it Head!
 void insertFirst(int key, int data) {
   struct node *link = (struct node *)malloc(sizeof(struct node));
   link->key = key;
@@ -22,6 +23,7 @@ void insertFirst(int key, int data) {
   head = link;
 }
 
+// Returns the length of list!
 int length() {
   int len = 0;
 
@@ -34,6 +36,7 @@ int length() {
   return len;
 }
 
+// Search and Finds a Node with Given key!
 struct node *find(int key) {
   struct node *current = head;
   if (head == NULL) {
@@ -51,6 +54,7 @@ struct node *find(int key) {
   return current;
 }
 
+// Delete a Node of Given key!
 struct node *deleteOne(int key) {
   struct node *current = head;
   struct node *previous = NULL;
@@ -74,6 +78,7 @@ struct node *deleteOne(int key) {
   return current;
 }
 
+// Delete the first Node!
 struct node *deleteFirst() {
   // saving refference of first node;
   struct node *tempLink = head;
@@ -81,6 +86,7 @@ struct node *deleteFirst() {
   return tempLink;
 }
 
+// Function for printing the List.
 void printList() {
   struct node *ptr = head;
   // printing list [1,2,3,3,4] like this!
@@ -90,7 +96,49 @@ void printList() {
     ptr = ptr->next;
   }
 
-  printf("]");
+  printf("]\n");
+}
+
+// Sorting the Linked List!
+void sort() {
+  struct node *current, *next;
+  int size = length() - 1;
+  int k = size + 1;
+
+  for (int i = 0; i < size; i++, k--) {
+    current = head;
+    next = current->next;
+
+    for (int j = 1; j < k; ++j) {
+      if (current->data > next->data) {
+        int tempData = current->data;
+        current->data = next->data;
+        next->data = tempData;
+        // Swap keys also!
+        int tempKey = current->key;
+        current->key = next->key;
+        next->key = tempKey;
+      }
+      current = current->next;
+      next = next->next;
+    }
+  }
+}
+
+void insertLast(int key, int data) {
+  node *newNode = (node *)malloc(sizeof(node));
+  newNode->key = key;
+  newNode->data = data;
+
+  node *current = head;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+
+  if (current->next == NULL) {
+    current->next = newNode;
+    newNode->next = NULL;
+  }
 }
 
 int main() {
@@ -100,11 +148,11 @@ int main() {
   insertFirst(4, 40);
   insertFirst(5, 12);
   insertFirst(6, 16);
-
   printList();
-  printf("\n%d", deleteFirst()->data);
+  printf("\nAfter Sort: ");
+  sort();
   printList();
-
-  printf("\nlength of list: %d", length());
-  printf("\n%d", find(6)->data);
+  printf("\nInsert at the END: ");
+  insertLast(8, 25);
+  printList();
 }
